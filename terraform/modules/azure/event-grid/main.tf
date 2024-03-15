2fe8cb019ebde1f7d34c2a2a1f5b09e5f7f815b4
SpiritZhou <iammrzhouzhenghan@gmail.com>
2024-03-15 12:21:32 +0800

    Update

--- terraform/modules/azure/event-grid/main.tf
+++ terraform/modules/azure/event-grid/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_eventgrid_topic" "eventgrid" {
 
 resource "azurerm_eventgrid_event_subscription" "eventsubscription" {
   name  = local.event_grid_subscription_name
-  scope = data.azurerm_eventgrid_topic.eventgrid.id
+  scope = azurerm_eventgrid_topic.eventgrid.id
   event_delivery_schema = "CloudEventSchemaV1_0"
 
   service_bus_topic_endpoint_id = "/subscriptions/66a7fcba-dac3-46d8-b509-202f305618c8/resourceGroups/keda-e2e-infrastructure-retry/providers/Microsoft.ServiceBus/namespaces/kedaretry2-e2e-servicebus-namespace-event-grid/topics/kedaretry2-e2e-receive-event-grid-topic"
